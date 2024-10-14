808b297629486447212c89cec9e1856baeaaa615
roshannayak1 <roshannayak792@gmail.com>
2024-10-14 15:35:31 +0530

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Image, StyleSheet, Text, View ,Button} from 'react-native'
+import React, { useState } from 'react'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const [ count ,Setcount]=useState(0);
+  const [ imageuri ,Setimageuri]=useState('https://picsum.photos/200/300');
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const Handlepress =()=>{
+    Setcount(count+1);
+    Setimageuri(`https://picsum.photos/200/300?random=${Math.random()}`);
+  }
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <>
+    <View style={styles.container}>
+      {/* <Text style={styles.text}>{name}</Text> */}
+      <Text style={styles.text}>{count}</Text>
+          {/* <Image source={require('../androidapp1/components/images/image1.png')} style={{width:100 ,height:100}} /> */}
+          {/* <Image source={{uri:'https://picsum.photos/id/237/200/300'}} style={{width:100 ,height:100}} /> */}
+          {/* <Image source={{uri:'https://picsum.photos/id/237/200/300'}} style={{width:100 ,height:100}} /> */}
+          <Image source={{uri:imageuri}} style={{width:200 ,height:200}} />
+     
     </View>
-  );
+    <View style={styles.button}>
+    <Button
+      title='NEXT IMAGE'
+    onPress={
+        Handlepress
+      }
+     />
+    </View>
+    </>
+  )
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+export default App
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container :{
+    flex:1,
+    justifyContent:"center",
+    alignItems:'center',
+    backgroundColor:'#f5f5f5'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  text:{
+    fontSize:24,
+    color:'#333',
 
-export default App;
+  },
+  button :{
+    marginTop:1 
+  }
+})
